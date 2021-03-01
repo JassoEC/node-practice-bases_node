65ae8c0a7773905feac212279f380b430246fc20
Emanuel Campos Jasso <62405903+EmanuelSulivan@users.noreply.github.com>
2021-03-01 10:44:22 -0600

    incio de proyecto de aplicacion de consola

---
+++ app.js
@@ -0,0 +1,11 @@
+console.clear();
+console.log(`
+=================
+  Tabla del 5
+=================
+`);
+const base = 2;
+for (let i = 1; i <= 10; i++) {
+  let operator = base * i;
+  console.log(`Resultado: ${operator}`);
+}
