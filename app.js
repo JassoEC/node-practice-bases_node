86d7471395ed06a5b89f18936b0d9203fd9717ba
Emanuel Campos Jasso <62405903+EmanuelSulivan@users.noreply.github.com>
2021-03-02 10:23:50 -0600

    desestructuracion de parametros de consola

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const { createFile } = require("./helpers/multiply");
-
 console.clear();
-const base = 8;
+
+const [, , baseParam = "--base=5"] = process.argv;
+const [, base = 5] = baseParam.split("=");
 
 createFile(base)
   .then((response) => console.log(response, " creado"))
